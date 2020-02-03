dbd601dcad26570df8de903eeae93d554b111e32
wellingtonlopes <ookami.seta@gmail.com>
2020-02-03 10:32:53 -0400

    added the first models of the project (game,genre,developer,publisher,platform) and configured the admin page to modify them

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Genre, Game, Publisher, Developer, Platform
 
 # Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Genre, GenreAdmin)
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Publisher, PublisherAdmin)
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Developer, DeveloperAdmin)
+
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Platform, PlatformAdmin)
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genres', 'platforms','developer')
+
+admin.site.register(Game, GameAdmin)
--- games/models.py
+++ games/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Developer(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField(default=timezone.now)
+    genre = models.ManyToManyField(Genre)
+    platform = models.ManyToManyField(Platform)
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT)
+    developer = models.ForeignKey(Developer,on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    # methods below are used to display all 'ManyToMany' values of each field
+    def genres(self):
+        return ", ".join([g.name for g in self.genre.all()])
+
+    def platforms(self):
+        return ", ".join([p.name for p in self.platform.all()])
