4cccea5e6ce27a5c00cc4ee8291a0814e12de948
wellingtonlopes <ookami.seta@gmail.com>
2020-02-05 10:25:38 -0400

    using the game title as an url endpoint and raising 404 if it does not exist

--- games/views.py
+++ games/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 # Create your views here.
 def index(request):
     games = Game.objects.all()
-    return render(request, 'games/index.html', { 'games': games })
+    return render(request, 'games/index.html', {'games': games})
+
+def detail(request, game_title):
+    game = get_object_or_404(Game.objects, title=game_title)
+    return render(request, 'games/detail.html', {'game': game}) 
